25434f1438d50fd474e6fe036f84f76d17bb2c1e
FacundoCollares <facucollares12@hotmail.com>
2024-10-03 15:24:56 -0300

    cambios

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,53 +1,54 @@
-const carteleraContent = document.getElementById('cartelera-content');
+const carteleraContent = document.getElementById("cartelera-content");
 
-tempData.map(pelicula => {
-  
-  const movieCard = document.createElement('article');
-  movieCard.classList.add('movie-card');
 
-  
-  const moviePoster = document.createElement('img');
-  moviePoster.src = pelicula.poster;
-  moviePoster.alt = pelicula.title;
-  moviePoster.classList.add('movie-poster');
-  movieCard.appendChild(moviePoster);
+$.get("https://students-api.up.railway.app/movies", (data)=>{
+  renderCards(data)
 
-  
-  const movieTitle = document.createElement('h3');
-  movieTitle.textContent = pelicula.title;
-  movieTitle.classList.add('movie-title');
-  movieCard.appendChild(movieTitle);
+})
 
-  
-  const movieYear = document.createElement('p');
-  movieYear.textContent = pelicula.year;
-  movieYear.classList.add('movie-year');
-  movieCard.appendChild(movieYear);
+const renderCards = (data) => {
+  data.map((pelicula) => {
+    const movieCard = document.createElement("article");
+    movieCard.classList.add("movie-card");
 
-  
-  const movieDirector = document.createElement('p');
-  movieDirector.innerHTML = `<strong>Director:</strong> ${pelicula.director}`;
-  movieDirector.classList.add('movie-director');
-  movieCard.appendChild(movieDirector);
+    const moviePoster = document.createElement("img");
+    moviePoster.src = pelicula.poster;
+    moviePoster.alt = pelicula.title;
+    moviePoster.classList.add("movie-poster");
+    movieCard.appendChild(moviePoster);
 
-  
-  const movieDuration = document.createElement('p');
-  movieDuration.innerHTML = `<strong>Duración:</strong> ${pelicula.duration}`;
-  movieDuration.classList.add('movie-duration');
-  movieCard.appendChild(movieDuration);
+    const movieTitle = document.createElement("h3");
+    movieTitle.textContent = pelicula.title;
+    movieTitle.classList.add("movie-title");
+    movieCard.appendChild(movieTitle);
 
-  
-  const movieRate = document.createElement('p');
-  movieRate.innerHTML = `<strong>Rating:</strong> ${pelicula.rate}`;
-  movieRate.classList.add('movie-rate');
-  movieCard.appendChild(movieRate);
+    const movieYear = document.createElement("p");
+    movieYear.textContent = pelicula.year;
+    movieYear.classList.add("movie-year");
+    movieCard.appendChild(movieYear);
 
-  
-  const movieGenre = document.createElement('p');
-  movieGenre.innerHTML = `<strong>Géneros:</strong> ${pelicula.genre.join(', ')}`;
-  movieGenre.classList.add('movie-genre');
-  movieCard.appendChild(movieGenre);
+    const movieDirector = document.createElement("p");
+    movieDirector.innerHTML = `<strong>Director:</strong> ${pelicula.director}`;
+    movieDirector.classList.add("movie-director");
+    movieCard.appendChild(movieDirector);
 
-  
-  carteleraContent.appendChild(movieCard);
-});
+    const movieDuration = document.createElement("p");
+    movieDuration.innerHTML = `<strong>Duración:</strong> ${pelicula.duration}`;
+    movieDuration.classList.add("movie-duration");
+    movieCard.appendChild(movieDuration);
+
+    const movieRate = document.createElement("p");
+    movieRate.innerHTML = `<strong>Rating:</strong> ${pelicula.rate}`;
+    movieRate.classList.add("movie-rate");
+    movieCard.appendChild(movieRate);
+
+    const movieGenre = document.createElement("p");
+    movieGenre.innerHTML = `<strong>Géneros:</strong> ${pelicula.genre.join(
+      ", "
+    )}`;
+    movieGenre.classList.add("movie-genre");
+    movieCard.appendChild(movieGenre);
+
+    carteleraContent.appendChild(movieCard);
+  });
+};
